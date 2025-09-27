405c6c19a0c607a9cdb38c5488fd06430f7b7b39
JNickUser <juanmejia0809@gmail.com>
2025-09-26 22:47:24 -0500

    Minor changes

--- install_chroot.sh
+++ install_chroot.sh
@@ -25,11 +25,11 @@ grub-mkconfig -o /boot/grub/grub.cfg
 
 # Setting hardware clock from system clock
 timedatectl set-timezone "America/Bogota"
+timedatectl set-ntp true
 hwclock --systohc
 
 # Configuring the Locales
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-echo "es_CO.UTF-8 UTF-8" >> /etc/locale.gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "KEYMAP=la-latin1" > /etc/vconsole.conf
 echo "XKBLAYOUT=latam" >> /etc/vconsole.conf
